21301c68a002e061a0c810d87c7c81b46e81abce
Matt <matt.voget@gmail.com>
2016-12-07 22:41:38 -0500

    changed to login form authentication

--- src/main/java/com/mattvoget/infomanager/config/WebSecurityConfig.java
+++ src/main/java/com/mattvoget/infomanager/config/WebSecurityConfig.java
@@ -1,21 +1,14 @@
 package com.mattvoget.infomanager.config;
 
-import com.mattvoget.infomanager.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.web.csrf.CsrfFilter;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private CustomUserDetailsService customUserDetailsService;
 
     @Autowired
     private CustomAuthenticationProvider authProvider;
@@ -28,9 +21,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().anyRequest().authenticated()
-                .and()
-                .httpBasic()
+            .authorizeRequests().anyRequest().authenticated()
+            .and().csrf().disable()
+            .formLogin().loginProcessingUrl("/login")
+            .usernameParameter("username").passwordParameter("password")
+            .and()
+            .logout()
+            .permitAll()
         ;
     }
 
