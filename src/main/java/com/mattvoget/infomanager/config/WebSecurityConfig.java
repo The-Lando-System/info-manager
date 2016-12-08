fd006ea0f11edc7a2cefa0dfc824a24edfdf841c
Matt <matt.voget@gmail.com>
2016-12-07 23:32:23 -0500

    changed back to http basic

--- src/main/java/com/mattvoget/infomanager/config/WebSecurityConfig.java
+++ src/main/java/com/mattvoget/infomanager/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -20,15 +21,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests().anyRequest().authenticated()
-            .and().csrf().disable()
-            .formLogin().loginProcessingUrl("/login")
-            .usernameParameter("username").passwordParameter("password")
-            .and()
-            .logout()
-            .permitAll()
-        ;
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
     }
 
 }
