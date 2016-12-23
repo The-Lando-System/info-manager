c19d7981083eb401515ad1c403800b8676dd22eb
Matt <matt.voget@gmail.com>
2016-12-22 21:11:27 -0700

    added cors

--- src/main/java/com/mattvoget/infomanager/config/WebSecurityConfig.java
+++ src/main/java/com/mattvoget/infomanager/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import com.mattvoget.sarlacc.client.authentication.AdminFilter;
 import com.mattvoget.sarlacc.client.authentication.SarlaccAuthProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.support.SimpleAutowireCandidateResolver;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,6 +15,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @ComponentScan({"com.mattvoget.sarlacc.client"})
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -53,6 +57,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests().anyRequest().authenticated()
             .and()
             .httpBasic();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 
 }
